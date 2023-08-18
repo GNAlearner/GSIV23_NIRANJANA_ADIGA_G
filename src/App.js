0d48ff43355b456a2575c9faf6c7826963cd3b6b
GNAlearner <g.niranjana.adiga@gmail.com>
2023-08-18 11:55:24 +0530

    Routes Installed and Implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import DetailsPage from "./components/DetailsPage";
+import ListPage from "./components/ListPage";
 import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
     <NavBar />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<ListPage/>}/>
+        <Route exact path="/details" element={<DetailsPage/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import SearchIcon from '@mui/icons-material/Search';
-import HomeIcon from '@mui/icons-material/Home';
+import { Search, Home } from '@mui/icons-material';
 
 const NavBar = () => {
     return (
         <nav className="navbar shadow shadow-sm">
             <div className="input-box ps-2 ms-2">
-                <SearchIcon />
+                <Search />
                 <input type="search" placeholder="Search" />
             </div>
-            <HomeIcon className="home-btn me-2"/>
+            <Home className="home-btn me-2"/>
         </nav>
     )
 }
